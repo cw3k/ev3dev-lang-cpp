dddfeb50115f427223120a43622b0fee40f5abd6
wcb <winston.bowen@fmc-na.com>
2022-08-15 16:47:52 -0400

    - initial commit of CRobotLocalizor class

---
+++ demos/CRobotLocalizor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wcb on 8/15/22.
+//
+
+#include "CRobotLocalizor.h"
---
+++ demos/CRobotLocalizor.h
@@ -0,0 +1,14 @@
+//
+// Created by wcb on 8/15/22.
+//
+
+#pragma once
+
+
+class CRobotLocalizor
+{
+
+};
+
+
+
